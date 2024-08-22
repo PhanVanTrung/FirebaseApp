9a288c191ab8ee53c44bbf65ae55ae726fb15825
TrungPV <PhanVanTrung@users.noreply.github.com>
2024-08-22 14:24:23 +0700

    add secrets (#4)

--- firebaseConfig.js
+++ firebaseConfig.js
@@ -1,15 +1,5 @@
-// const firebaseConfig = {
-//     apiKey: "AIzaSyDc--387sq_sqsmMWzTIzQvZd-g4-aVMKY",
-//     authDomain: "gsmtest-9f523.firebaseapp.com",
-//     databaseURL: "https://gsmtest-9f523-default-rtdb.firebaseio.com",
-//     projectId: "gsmtest-9f523",
-//     storageBucket: "gsmtest-9f523.appspot.com",
-//     messagingSenderId: "30656126464",
-//     appId: "1:30656126464:web:a925a0e4d20a5a32a442e5",
-//   };
-
 const firebaseConfig = {
-    apiKey: "AIzaSyB7IVQ4yiqS_DvZc-7SgVQO8LdVCyO2SCw",
+    apiKey: "env.secrets.API_KEY",
     authDomain: "giamsattudien.firebaseapp.com",
     databaseURL: "https://giamsattudien-default-rtdb.firebaseio.com",
     projectId: "giamsattudien",
@@ -18,6 +8,18 @@ const firebaseConfig = {
     appId: "1:554091071071:web:02febdb64b6abeef063dea",
     measurementId: "G-NC4QZCTBTF"
   };
+
+//   const firebaseConfig = {
+//     apiKey: "${{ secrets.API_KEY }}",
+//     authDomain: "${{ secrets.AUTH_DOMAIN }}",
+//     databaseURL: "${{ secrets.DATABASE_URL }}",
+//     projectId: "${{ secrets.PROJECT_ID }}",
+//     storageBucket: "${{ secrets.STORAGE_BUCKET }}",
+//     messagingSenderId: "${{ secrets.MESSAGING_SENDER_ID }}",
+//     appId: "${{ secrets.APP_ID }}",
+//     measurementId: "${{ secrets.MEASUREMENT_ID }}"
+//   };
+
 
 const testEmail = {
     username: "testuser@gmail.com",
